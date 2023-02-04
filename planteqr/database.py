67bba5452b28fde408ab92ea94e8d0ec296fd6d2
CAMARM_Flipz <armand.camponovo@icloud.com>
2023-02-04 16:57:11 +0100

    enlèvement d'un "print"

--- planteqr/database.py
+++ planteqr/database.py
@@ -11,7 +11,6 @@ class Database:
         return client.Plantes
 
     def get_database_url(self):
-        print(self.config.sections())
         mongo_user = self.config['mongodb.net']['mongo_user']
         mongo_password = self.config['mongodb.net']['mongo_password']
         mongo_server = self.config['mongodb.net']['mongo_server']
