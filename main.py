0a5888cf8a707bab66af848368cf680d898f0e98
CAMARM_Flipz <armand.camponovo@icloud.com>
2023-02-04 16:57:38 +0100

    commencement de la page d'accueil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
 from configparser import ConfigParser
-import pymongo
 from planteqr import Database
 
 app = Flask('Une e-plante')
@@ -9,5 +8,10 @@ config.read('config.ini')
 database = Database(config).get()
 
 
+@app.route('/')
+def accueil():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
